f63e9497184d3c154d4ee6a58be4824bbfec49fa
sakamoto-waka <wakanmhl@gmail.com>
2022-03-27 14:18:36 +0000

    [add] routing books

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-
+  resources :books, only: [:index, :show]
 end
